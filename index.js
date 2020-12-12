f76ce6e6b28c7a4904d850e1e1e556a04ec27bc2
Anna Hangstefer <anna-hangstefer@lambdastudents.com>
2020-12-12 12:42:38 -0500

    index.js, .env, routes

--- index.js
+++ index.js
@@ -12,3 +12,20 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Go code!
 */
+
+const express =require("express")
+const actionRouter = require("./api/actions/actions-router")
+const projectRouter =require("./api/projects/projects-router")
+
+const server = express();
+const port = 5000;
+
+server.use(express.json())
+server.use('/api/actions', actionRouter);
+server.use('/api/project', projectRouter)
+
+server.listen(port, ()=>{
+    console.log(`Server is running at http://localhost:${port}`)
+})
+
+module.exports = server
