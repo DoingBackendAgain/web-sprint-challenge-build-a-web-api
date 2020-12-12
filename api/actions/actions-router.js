fed29978b74b4ce243b7264c94ebe2180f37db07
Anna Hangstefer <anna-hangstefer@lambdastudents.com>
2020-12-12 17:00:19 -0500

    actions router complete

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -38,6 +38,65 @@ router.get("/:id", (req, res)=> {
 })
 
 
+router.post("/", (req, res)=> {
+    if(!req.body.description || !req.body.notes){
+        return res.status(404).json({
+            message: "please add description and notes"
+        })
+    }      
+    aModel.insert(req.body)
+        .then((actions) => {
+            res.status(201).json(actions)
+        })
+        .catch((err)=> {
+            console.log(err)
+            return res.status(500).json({
+                message: "error adding your action"
+            })
+        })
+})
+//in insomnia, you must add project_id to json
+
+
+
+router.put("/:id", (req, res)=> {
+ aModel.update(req.params.id, req.body)
+    .then((action)=> {
+        if(action){
+            res.status(200).json(action)
+        }
+        else {
+            res.status(404).json({
+                message: "This project doesn't exist"
+            })
+        }
+    })
+    .catch((err)=> {
+        console.log(err)
+    })
+})
+
+
+
+
+router.delete("/:id", (req, res)=> {
+    aModel.remove(req.params.id)
+        .then((action)=> {
+            if(action){
+                return res.status(200).json({
+                    message: "You've forever destroyed an action"
+                })
+            }
+            else {
+                res.status(400).json({
+                    message: "You can't get no action here"
+                })
+            }
+        })
+        .catch((err)=>{
+            console.log(err)
+        })
+})
 
 
 module.exports = router
