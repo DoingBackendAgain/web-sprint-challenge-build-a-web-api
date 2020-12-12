491bd26e7586e21cf8d45c9c9c19796b0b85618f
Anna Hangstefer <anna-hangstefer@lambdastudents.com>
2020-12-12 14:50:18 -0500

    adding .get to actions router

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -4,7 +4,38 @@ const aModel = require("./actions-model")
 
 const router = express.Router()
 
+router.get("/", (req, res)=> {
+    aModel.get(req.params.id)
+        .then((actions)=> {
+            if(!actions){
+                return res.status(404).json({
+                    message: "There's no actions here"
+                })
+            }else{
+                res.status(200).json(actions)
+            }
+        })
+        .catch((err)=> {
+            console.log(err)
+        })
+})
 
+router.get("/:id", (req, res)=> {
+    aModel.get(req.params.id)
+        .then((actions)=> {
+            if(!actions){
+                return res.status(404).json({
+                    message: "There are no actions with this ID"
+                })
+            }
+            else {
+                res.status(200).json(actions)
+            }
+        })
+        .catch((err)=> {
+            console.log(err)
+        })
+})
 
 
 
