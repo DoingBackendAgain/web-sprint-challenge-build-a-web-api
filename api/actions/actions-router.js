eed090e21d7c6da1d08a9a44981381d7bd7b2337
Anna Hangstefer <anna-hangstefer@lambdastudents.com>
2020-12-12 13:02:51 -0500

    running

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,12 @@
 // Write your "actions" router here!
+const express = require("express")
+const aModel = require("./actions-model")
+
+const router = express.Router()
+
+
+
+
+
+
+module.exports = router
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,16 @@
 // Write your "projects" router here!
+const express = require("express")
+const pModel = require("./projects-model")
+const db = require("../../data/dbConfig")
+
+const router = express.Router()
+
+
+
+
+
+
+
+
+
+module.exports = router
